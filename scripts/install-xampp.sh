95b3e4ece4a1aca821d0d45d095b3eafedfaa59b
aaronhayes <aaron.hayes92@gmail.com>
2014-09-19 21:07:49 +1000

    Script to install Xampp on Mininet VM

---
+++ scripts/install-xampp.sh
@@ -0,0 +1,12 @@
+sudo wget http://sourceforge.net/projects/xampp/files/XAMPP%20Linux/1.8.3/xampp-linux-1.8.3-2-installer.run/download xampp.run
+sudo chmod +x xampp.run
+sudo ./xampp.run #just go with Y to all (could probably pipe yes in)
+
+
+# To Start Services
+sudo /opt/lampp/lampp start
+
+
+# To Stop Services
+#sudo /opt/lampp/lampp stop
+
