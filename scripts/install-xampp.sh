6c045fce96d2e6f67537bd23d4cf7fc6f4e50698
Aaron Hayes <aaron.hayes92@gmail.com>
2014-09-20 13:41:43 +1000

    update install

--- scripts/install-xampp.sh
+++ scripts/install-xampp.sh
@@ -1,6 +1,6 @@
-sudo wget http://sourceforge.net/projects/xampp/files/XAMPP%20Linux/1.8.3/xampp-linux-1.8.3-2-installer.run/download xampp.run
-sudo chmod +x xampp.run
-sudo ./xampp.run #just go with Y to all (could probably pipe yes in)
+sudo wget http://sourceforge.net/projects/xampp/files/XAMPP%20Linux/1.8.3/xampp-linux-1.8.3-2-installer.run/download
+sudo chmod +x download
+sudo ./download #just go with Y to all (could probably pipe yes in)
 
 
 # To Start Services
