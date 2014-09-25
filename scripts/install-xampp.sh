6351e2ed30d8505050d85330271b26fb2f57627b
shalvin <shalvin.deo@live.com>
2014-09-25 01:48:43 -0700

    Updated install script.
    Made changes to the install script as it wasn't working.
    The version of xampp is 64-bit and probably a different version from before. We should use the 64-bit version as the mininet image is 64-bit.

--- scripts/install-xampp.sh
+++ scripts/install-xampp.sh
@@ -1,11 +1,16 @@
-sudo wget http://sourceforge.net/projects/xampp/files/XAMPP%20Linux/1.8.3/xampp-linux-1.8.3-2-installer.run/download
-sudo chmod +x download
-sudo ./download #just go with Y to all (could probably pipe yes in)
-
+# Should avoid using sudo unless its neccessary
+# cd to home directory where we can freely write.
+cd ~
+# Download the installer. This is the 64-bit version as the mininet image is 64-bit.
+wget http://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/1.8.3/xampp-linux-x64-1.8.3-5-installer.run
+# We wrote the file and own it so no need for sudo here. Make the file executable.
+chmod +x xampp-linux-x64-1.8.3-5-installer.run
+# Run the executable installer. Need higher permissions.
+# Manually select yes (Y) for each option & hit enter each time.
+sudo ./xampp-linux-x64-1.8.3-5-installer.run
 
 # To Start Services
 sudo /opt/lampp/lampp start
-
 
 # To Stop Services
 #sudo /opt/lampp/lampp stop
